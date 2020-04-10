487b7d89ca3d87b935bd7f8b0cb1edc6a33a1ff1
lijuan <2420082764@qq.com>
2020-04-10 14:48:50 +0800

    fix indent

--- Live-Streaming/Agora-Interactive-Broadcasting-Live-Streaming-Web-Webpack/.eslintrc.js
+++ Live-Streaming/Agora-Interactive-Broadcasting-Live-Streaming-Web-Webpack/.eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true,
-		"node": true,
-		"jquery": true,
+	'env': {
+			'browser': true,
+			'es6': true,
+			'node': true,
+			'jquery': true,
 	},
-	"extends": "eslint:recommended",
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
+	'extends': 'eslint:recommended',
+	'globals': {
+			'Atomics': 'readonly',
+			'SharedArrayBuffer': 'readonly'
 	},
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
+	'parserOptions': {
+			'ecmaVersion': 2018,
+			'sourceType': 'module'
 	},
-	"rules": {
-		"indent": [
-			"warn",
-			"tab"
-		],
-		"linebreak-style": [
-			"warn",
-			"unix"
-		],
-		"quotes": [
-			"warn",
-			"double"
-		],
-		"semi": [
-			"warn",
-			"never"
-		],
-		"no-unused-vars": 0,
+	'rules': {
+			'indent': [
+					'warn',
+					2
+			],
+			'linebreak-style': [
+					'warn',
+					'unix'
+			],
+			'quotes': [
+					'warn',
+					'single'
+			],
+			'semi': [
+					'warn',
+					'never'
+			],
+			'no-unused-vars': 0,
 	}
 }
